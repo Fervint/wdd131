8161d193adefd369178b3b7dfb5ede15ce1dd952
Fervint <fricardodasilvamuniz@byupathway.edu>
2026-07-03 22:44:35 -0300

    feat: add script to display last update date in footer

--- semana01/obterdatas.js
+++ semana01/obterdatas.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Script obterdatas.js carregado com sucesso!");
+    console.log("Script obterdata.js carregado com sucesso!");
     const footer = document.querySelector("footer p:last-child");
     const dataAtual = new Date().toLocaleDateString("pt-BR");
     footer.insertAdjacentHTML("beforeend", `<br>Última atualização: ${dataAtual}`);
