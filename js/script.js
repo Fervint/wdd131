303eaee7640dad85f5c5db4ff388247866725fd9
Fervint <fricardodasilvamuniz@byupathway.edu>
2026-07-31 18:26:59 -0300

    feat: atualizar estilo do formulário e adicionar cabeçalho ao HTML

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// Preenche dinamicamente o campo de produtos
 const produtos = [
   { id: "fc-1888", nome: "capacitor de fluxo", classificacaomedia: 4.5 },
   { id: "fc-2050", nome: "fios elétricos", classificacaomedia: 4.7 },
